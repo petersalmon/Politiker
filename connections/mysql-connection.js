267818c24797389fcdbf702d4558260a1d6452bc
Peter Salamon <petersalamon14@gmail.com>
2020-07-29 21:14:01 -0400

    Updating MySQL connection

--- connections/mysql-connection.js
+++ connections/mysql-connection.js
@@ -1,11 +1,19 @@
 const mysql = require("mysql");
 require("dotenv/config");
 
+// var mysqlConnection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: process.env.MYSQL_PASSWORD,
+//     database: "nyc",
+//     multipleStatements: true
+// });
+
 var mysqlConnection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: process.env.MYSQL_PASSWORD,
-    database: "nyc",
+    host: "us-cdbr-east-02.cleardb.com",
+    user: "b085f4726117c1",
+    password: process.env.HEROKU_MYSQL_PASSWORD,
+    database: "heroku_47ea31f06bc3e94",
     multipleStatements: true
 });
 
