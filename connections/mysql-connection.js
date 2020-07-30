d21d5d6f9fa9f04bc74efc0484537dc507e4af12
Peter Salamon <petersalamon14@gmail.com>
2020-07-30 07:54:46 -0400

    Updaing mysql connection

--- connections/mysql-connection.js
+++ connections/mysql-connection.js
@@ -28,16 +28,12 @@ require("dotenv/config");
 //     multipleStatements: true
 // });
 
-var mysqlConnection;
-function connectDb() {
-    mysqlConnection  = mysql.createConnection({
-        host: "us-cdbr-east-02.cleardb.com",
-        user: "b085f4726117c1",
-        password: process.env.HEROKU_MYSQL_PASSWORD,
-        database: "heroku_47ea31f06bc3e94",
-        multipleStatements: true
-    });
-    connection.on('error', connectDb()); // probably worth adding timeout / throttle / etc
-}
+const mysqlConnection = mysql.createPool({
+    host: "us-cdbr-east-02.cleardb.com",
+    user: "b085f4726117c1",
+    password: process.env.HEROKU_MYSQL_PASSWORD,
+    database: "heroku_47ea31f06bc3e94",
+    multipleStatements: true
+});
 
 module.exports = mysqlConnection;
