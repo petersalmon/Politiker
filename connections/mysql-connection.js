846f0b51167ffaa44deab22aab7e5f44257b7d55
Peter Salamon <petersalamon14@gmail.com>
2020-07-29 22:37:22 -0400

    Updating mysql connection ... again

--- connections/mysql-connection.js
+++ connections/mysql-connection.js
@@ -9,6 +9,14 @@ require("dotenv/config");
 //     multipleStatements: true
 // });
 
+var mysqlConnection = mysql.createConnection({
+    host: "us-cdbr-east-02.cleardb.com",
+    user: "b085f4726117c1",
+    password: process.env.HEROKU_MYSQL_PASSWORD,
+    database: "heroku_47ea31f06bc3e94",
+    multipleStatements: true
+});
+
 // mysqlConnection.connect(function(err){
 //     if(err){
 //         console.log(err);
@@ -18,59 +26,4 @@ require("dotenv/config");
 //     }
 // });
 
-
-function initializeConnection(config) {
-    function addDisconnectHandler(connection) {
-        connection.on("error", function (error) {
-            if (error instanceof Error) {
-                if (error.code === "PROTOCOL_CONNECTION_LOST") {
-                    console.error(error.stack);
-                    console.log("Lost connection. Reconnecting...");
-
-                    initializeConnection(connection.config);
-                } else if (error.fatal) {
-                    throw error;
-                }
-            }
-        });
-    }
-
-    var connection = mysql.createConnection(config);
-
-    // Add handlers.
-    addDisconnectHandler(connection);
-
-    connection.connect();
-    return connection;
-}
-
-var connection = initializeConnection({
-    host: "us-cdbr-east-02.cleardb.com",
-    user: "b085f4726117c1",
-    password: process.env.HEROKU_MYSQL_PASSWORD,
-    database: "heroku_47ea31f06bc3e94",
-    multipleStatements: true
-});
-
-function handleDisconnect(connection) {
-    connection.on('error', function(err) {
-      if (!err.fatal) {
-        return;
-      }
-  
-      if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-        throw err;
-      }
-  
-      console.log('Re-connecting lost connection: ' + err.stack);
-  
-      connection = mysql.createConnection(connection.config);
-      handleDisconnect(connection);
-      connection.connect();
-    });
-}
-  
-handleDisconnect(connection);
-
-
-module.exports = connection;
+module.exports = mysqlConnection;
