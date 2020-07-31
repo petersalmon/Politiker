f014cf90b415f6992ef6087e8971921c0531756b
Peter Salamon <petersalamon14@gmail.com>
2020-07-31 08:52:07 -0400

    Updating mongoose connection

--- connections/mongo-connection.js
+++ connections/mongo-connection.js
@@ -1,15 +1,31 @@
 const mongoConnection = require("mongoose");
 require("dotenv/config");
 
-// Connect to Mongo DB 
-mongoConnection.connect(process.env.MONGODB_CONNECTION, 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    },
-    function(){
-        console.log("You are now connected to the Mongoose database!");
+// // Connect to Mongo DB 
+// mongoConnection.connect(process.env.MONGODB_CONNECTION, 
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     },
+//     function(){
+//         console.log("You are now connected to the Mongoose database!");
+//     }
+// );
+
+const db = process.env.MONGODB_CONNECTION;
+
+const mongoConnection = async () => {
+    try {
+      await mongoose.connect(db, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+      });
+      console.log("You are now connected to the Mongoose database!");
+    } catch (err) {
+        console.error("Something went wrong! Connection to the Mongoose database failed.");
+        console.error(err.message);
+        process.exit(1);
     }
-);
+};
 
 module.exports = mongoConnection;
