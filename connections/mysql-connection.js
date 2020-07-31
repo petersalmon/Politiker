a26cbdb95e9716318a86592feb5252bdab380dc7
Peter Salamon <petersalamon14@gmail.com>
2020-07-31 08:36:43 -0400

    Updating mysql connection

--- connections/mysql-connection.js
+++ connections/mysql-connection.js
@@ -1,39 +1,31 @@
 const mysql = require("mysql");
 require("dotenv/config");
 
-// var mysqlConnection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: process.env.MYSQL_PASSWORD,
-//     database: "nyc",
-//     multipleStatements: true
-// });
-
-// mysqlConnection.connect(function(err){
-//     if(err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log("You are now connected to the nyc MySQL database!");
-//     }
-// });
-
-// var mysqlConnection
-
-//  mysqlConnection = mysql.createConnection({
-//     host: "us-cdbr-east-02.cleardb.com",
-//     user: "b085f4726117c1",
-//     password: process.env.HEROKU_MYSQL_PASSWORD,
-//     database: "heroku_47ea31f06bc3e94",
-//     multipleStatements: true
-// });
-
-const mysqlConnection = mysql.createPool({
+const dbConnectionInfo = {
     host: "us-cdbr-east-02.cleardb.com",
     user: "b085f4726117c1",
     password: process.env.HEROKU_MYSQL_PASSWORD,
     database: "heroku_47ea31f06bc3e94",
     multipleStatements: true
+}
+
+var dbconnection = mysql.createPool(
+    dbConnectionInfo
+); 
+
+// Attempt to catch disconnects 
+dbconnection.on('connection', function (connection) {
+
+    console.log('Database Connection established');
+  
+    connection.on('error', function (err) {
+      console.error(new Date(), 'MySQL error', err.code);
+    });
+    
+    connection.on('close', function (err) {
+      console.error(new Date(), 'MySQL close', err);
+    });
+  
 });
 
 module.exports = mysqlConnection;
