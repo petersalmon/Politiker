00e803e53e637a1fe60cd1810278f0482e649fc5
Peter Salamon <petersalamon14@gmail.com>
2020-08-04 08:25:33 -0400

    Updating users.js

--- connections/mongo-connection.js
+++ connections/mongo-connection.js
@@ -1,31 +1,44 @@
 const mongoConnection = require("mongoose");
 require("dotenv/config");
 
-// // Connect to Mongo DB 
-// mongoConnection.connect(process.env.MONGODB_CONNECTION, 
-//     {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     },
-//     function(){
-//         console.log("You are now connected to the Mongoose database!");
-//     }
-// );
-
-const db = process.env.MONGODB_CONNECTION;
-
-const mongoDbConnection = async () => {
-    try {
-      await mongoose.connect(db, {
+// Connect to Mongo DB 
+mongoConnection.connect(process.env.MONGODB_CONNECTION, 
+    {
+        useNewUrlParser: true,
         useUnifiedTopology: true,
-        useNewUrlParser: true
-      });
-      console.log("You are now connected to the Mongoose database!");
-    } catch (err) {
-        console.error("Something went wrong! Connection to the Mongoose database failed.");
-        console.error(err.message);
-        process.exit(1);
+        server: { 
+            socketOptions: { 
+              keepAlive: 300000, connectTimeoutMS: 30000 
+            } 
+        }, 
+          replset: { 
+            socketOptions: { 
+              keepAlive: 300000, 
+              connectTimeoutMS : 30000 
+            } 
+        }
+    },
+    function(){
+        console.log("You are now connected to the Mongoose database!");
     }
-};
+);
 
-module.exports = mongoDbConnection;
+// const db = process.env.MONGODB_CONNECTION;
+
+// const mongoDbConnection = async () => {
+//     try {
+//       await mongoose.connect(db, {
+//         useUnifiedTopology: true,
+//         useNewUrlParser: true
+//       });
+//       console.log("You are now connected to the Mongoose database!");
+//     } catch (err) {
+//         console.error("Something went wrong! Connection to the Mongoose database failed.");
+//         console.error(err.message);
+//         process.exit(1);
+//     }
+// };
+
+// module.exports = mongoDbConnection;
+
+module.exports = mongoConnection;
