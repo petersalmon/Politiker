ead1c915694d11672e94bdd89d9552cb3f1bee13
Peter Salamon <petersalamon14@gmail.com>
2020-08-07 20:53:26 -0400

    Updateed MySQL connection

--- connections/mysql-connection.js
+++ connections/mysql-connection.js
@@ -1,21 +1,12 @@
 const mysql = require("mysql");
 require("dotenv/config");
 
-const mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createPool({
     host: "us-cdbr-east-02.cleardb.com",
     user: "b085f4726117c1",
     password: process.env.HEROKU_MYSQL_PASSWORD,
     database: "heroku_47ea31f06bc3e94",
     multipleStatements: true
-});
-
-mysqlConnection.connect(function(err){
-  if(err){
-      console.log(err);
-  }
-  else{
-      console.log("You are now connected to the nyc MySQL database!");
-  }
 });
 
 module.exports = mysqlConnection;
